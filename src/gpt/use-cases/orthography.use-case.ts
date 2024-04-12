b35aa5dbd35cbdb56dd5e1a0eb069f12b243d9be
giovanni <manuelgio.cortazar.delacruz@gmail.com>
2024-04-12 00:40:04 -0600

    Implementación de openai y primer prompt de GPT

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,13 +1,28 @@
+import OpenAI from "openai";
+
 interface Options {
     prompt: string;
 }
 
-export const orthographyCheckUseCase = async( options: Options ) => {
-    
+export const orthographyCheckUseCase = async (openai: OpenAI, options: Options) => {
+
     const { prompt } = options;
 
-    return {
-        prompt: prompt,
-        apiKey: process.env.OPENAI_API_KEY,
-    }
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: "Eres un asistente para los empleados de ToolFast empresa de herramientas industriales y ayudarás a los empleados sobre cualquier duda amablemente y tu nombre es Airinto",
+            },
+            {
+                role: 'user',
+                content: prompt,
+            }
+        ],
+        model: "gpt-3.5-turbo",
+    });
+
+    console.log(completion);
+
+    return completion.choices[0];
 }
